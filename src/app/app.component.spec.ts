14c3ec01a36adb9fa8fec89a8226a25392ff0dd6
bobyDeveloper <zS21004509@estudiantes.uv.mx>
2023-08-25 14:09:34 -0600

    updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,19 +8,19 @@ describe('AppComponent', () => {
     declarations: [AppComponent]
   }));
 
-  Xit('should create the app', () => {
+  xit('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  Xit(`should have as title 'mycv'`, () => {
+  xit(`should have as title 'mycv'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('mycv');
   });
 
-  Xit('should render title', () => {
+  xit('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
