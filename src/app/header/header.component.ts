df0b8c9261da4dd9845947db049b57fdb764ec6a
bobyDeveloper <118419773+bobyDeveloper@users.noreply.github.com>
2023-09-06 19:20:59 -0600

    Header Closes #3 (#4)
    * update
    
    * xit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeaderService } from '../services/header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+	constructor(private headerService: HeaderService) {}
+
+	name : string = "fake name";
+	titulos : string = "fake phd";
+	mision : string = "fake mision";
+	foto : string = "fake photo";
+
+	ngOnInit(): void {
+		this.headerService.getHeader()
+		.subscribe( (data: any) => {
+			console.log(data);
+			//alert(data);
+			this.name = data.name;
+			this.titulos = data.titulos;
+			this.mision = data.mision;
+			this.foto = data.foto;
+		});
+	}
+
 
 }
